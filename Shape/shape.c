fd39b066d61814ce8fb4be5b5cd26761694db54b
Your Name <22kartiksanil@gmail.com>
2023-06-10 16:49:03 +0530

    Using Printf

---
+++ Shape/shape.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // We are printing a right-angled Triangle
+
+    printf("   /| \n");
+    printf("  / | \n");
+    printf(" /  | \n");
+    printf("/___| \n");
+
+    // Printing a Rectangle
+
+    printf("________\n");
+    printf("|      |\n");
+    printf("|      |\n");
+    printf("|      |\n");
+    printf("|      |\n");
+    printf("--------\n");
+
+    // Random
+
+    printf("....\n");
+    printf(".NK.\n");
+    printf("....\n");
+    return 0;
+}
