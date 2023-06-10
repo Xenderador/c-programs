ef70681405cc2bc73a3b5ce74116624ba018f002
Your Name <22kartiksanil@gmail.com>
2023-06-10 17:32:11 +0530

    Data Types

---
+++ Hello_World/Data Types/dt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    // Interger datatype
+
+    int a = 3;
+
+    // Float datatype (this print the numbers after the decimal point)
+
+    float b = 3.142;
+
+    // Char datatype
+
+    char c = 'J';
+    printf("\n %d \n %f \n %c", a, b, c);
+    return 0;
+}
